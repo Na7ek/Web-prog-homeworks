dca25e364737549b2b2b99a045d9b3d421daa4af
Na7ek <khatayannn007@gmail.com>
2025-09-21 15:48:53 +0400

    test

---
+++ Lesson4.js
@@ -0,0 +1,50 @@
+//#1
+// function oddSum(a){
+//     let sum=0
+//     for (let i=0; i<a; i++){
+//         if (i%2!==0){
+//             sum+=i;
+//         }
+//     }
+//     console.log(sum)
+// }
+// oddSum(12)
+
+//#2
+// function fivePatik(a){
+//     if(a%5==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(fivePatik(15))
+
+//#3
+// function ifBothZuyg(a,b){
+//     if(a%2==0 && b%2==0){
+//         console.log(a+b);
+//     }
+//     else{
+//         console.log(a*b);
+//     }
+// }
+// ifBothZuyg(13,4)
+
+//#4
+// function negOrPos(a){
+//     if (a>0){
+//         console.log("Your number is positive");
+//     }
+//     else if(a==0){
+//         console.log("Your number is 0");
+//     }
+//     else if(a<0){
+//         console.log("Your number is negative");
+//     }
+//     else{
+//         console.log("Not a number");
+//     }
+// }
+// negOrPos(4);
