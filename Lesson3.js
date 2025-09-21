9212718b54ca5f2d9cbe88c3e8ae07ecdfedc121
Na7ek <khatayannn007@gmail.com>
2025-09-21 15:25:05 +0400

    test

---
+++ Lesson3.js
@@ -0,0 +1,68 @@
+//#1
+// let n;
+// n=Number(prompt("Enter a number:"));
+// if(n>0){
+//     console.log(1)
+// }
+// else if(n<0){
+//     console.log(-1)
+// }
+// else{
+//     console.log(0)
+// }
+
+//#2
+// let result;
+// result = a+b<4 ? "Few" : "Many" //Էս չի աշխատելու, որովհետև a-ն ու b-ն undefined են, բայց syntaxy-ը ճիշտ է։
+
+//#3
+// for (let i=10;i<=40;i+=2){
+//     console.log(i);
+// }
+
+//#4
+// let a=Number(prompt("Enter a number:"));
+// for (let i=1;i<=a;i++){
+//     console.log(i*a);
+// }
+
+//#5
+// let sum=0;
+// let i=1;
+// while(i<=14){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum);
+
+//#6
+// let sum=0
+// for (let i=1; i<=40; i++){
+//     if(i%2==0){
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
+
+//#7
+// let sum=0;
+// let i=1;
+// while(i<=40){
+//     if(i%2!==0){
+//         sum+=i;
+//         i++;
+//     }
+//     else{
+//         i++;
+//     }
+// }
+// console.log(sum);
+
+//#8
+// let art=1;
+// for(let i=1;i<=60;i++){
+//     if(i%6==0){
+//         art*=i;
+//     }
+// }
+// console.log(art);
