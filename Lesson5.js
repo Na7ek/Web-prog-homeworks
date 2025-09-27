ed778b03d493bef2352c19ecb367b703def2ef99
Na7ek <khatayannn007@gmail.com>
2025-09-27 13:07:11 +0400

    test

---
+++ Lesson5.js
@@ -0,0 +1,28 @@
+// //#1
+
+// let person = {
+//     name: "Hovsep",
+//     age: 19,
+//     city: "Yerevan"
+// }
+// person.isStudent = true
+// delete person.city
+
+// console.log("age" in person)
+
+// console.log(person)
+
+// //#2
+
+// let obj = {
+// 	key1: "hello",
+// 	key2: undefined,
+// 	key3: 123,
+// 	key4: undefined
+// }
+// for (key in obj){
+//     if (obj[key]===undefined){
+//         delete obj[key]
+//     }
+// }
+// console.log(obj)
