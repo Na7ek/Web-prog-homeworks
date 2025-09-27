78118db748b10470ec712557b0628b99138a7168
Na7ek <khatayannn007@gmail.com>
2025-09-27 13:08:12 +0400

    test

---
+++ Lesson6.js
@@ -0,0 +1,53 @@
+//#1
+
+// let arr = ["banana", "orange"]
+// arr.push("apple", "pineapple")
+// arr.unshift("berry", "melon")
+// arr.pop()
+// arr.shift()
+// console.log(arr)
+
+//#2
+
+// const numbers = [3, 7, 10, 15, 20, 25]
+// let firstBiggerThanTen = numbers.find(item=>item>10)
+// let firstHngapatik = numbers.find(item=>item%5==0)
+// console.log(firstBiggerThanTen)
+// console.log(firstHngapatik)
+
+//#3
+
+// const numbers = [5, 12, 8, 130, 44]
+// let biggerThanTen = numbers.filter(item=>item>10)
+// let evenNums = numbers.filter(item=>item%2==0)
+// console.log(biggerThanTen)
+// console.log(evenNums)
+
+//#4
+
+// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"]
+// let longerThanSix = words.filter(item=>item.length>6)
+// console.log(longerThanSix)
+
+//#5
+
+// const numbers = [1, 2, 3, 4, 5]
+// let timesTwo = numbers.map(item=>item*2)
+// let sumUpWTen = numbers.map(item=>item+10)
+// console.log(timesTwo)
+// console.log(sumUpWTen)
+
+//#6
+
+// const people = [
+// { name: "Anna", age: 17 },
+// { name: "Karen", age: 22 },
+// { name: "Aram", age: 15 },
+// { name: "Mery", age: 19 }
+// ]
+// let firstOlderThan18 = people.find(item=>item.age>18)
+// let Adults = people.filter(item=>item.age>=18)
+// let onlyNames = people.map(item=>item.name)
+// console.log(firstOlderThan18)
+// console.log(Adults)
+// console.log(onlyNames)
