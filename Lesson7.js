57083f658c588b276c43f6ae7db935b8af5c8f46
Na7ek <khatayannn007@gmail.com>
2025-10-12 17:10:40 +0400

    test

---
+++ Lesson7.js
@@ -0,0 +1,80 @@
+// #1
+
+// let arr = [21, 42, 4, -11, -7, 26]
+// arr.splice(2, 1)
+// console.log(arr)
+
+// #2
+
+// let arr = [21, 42, 4, -11, -7, 26]
+// arr.splice(0,3, 56, 122, 25)
+// console.log(arr)
+
+// #3
+
+// let arr1 = [21, -7, 26]
+// let arr2 = ["apple", "banana", "peach"]
+// let arr = [];
+// arr = arr.concat(arr1,arr2)
+// arr.reverse()
+// console.log(arr)
+
+// #4
+
+// let str = "яблоко,банан,киви,груша"
+// let arr = []
+// arr = str.split(',')
+// console.log(arr)
+
+//#5
+
+// let arr = ["Я", "учу", "JS"]
+// let str = arr.join(' ')
+// console.log(str)
+
+// #6
+
+// let arr = ["html", "css", "js"];
+// console.log(arr.includes("js"))
+
+// #7
+
+// function strPresent(arr, str){
+//     if (arr.includes(str)){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// let arr = ["apple", "banana", "kiwi", "pear"]
+// let str = "banana"
+// console.log(strPresent(arr,str))
+
+// #8
+
+// let arr = [10, 20, 30, 40, 50]
+// arr.splice(0,3)
+// console.log(arr)
+
+// #9
+
+// let arr =  ["red", "green", "blue", "yellow"]
+// arr.splice(2,1,"purple")
+// console.log(arr)
+
+// #10
+
+// let str = "Привет как дела"
+// let revArr = []
+// revArr = str.split(' ')
+// revArr.reverse()
+// let revStr = revArr.join(' ')
+// console.log(revStr)
+
+// #11
+
+// let arr =  ["cat", "dog", "hamster", "parrot"]
+// let hasRs = arr.filter(item => item.includes('r'))
+// let hasRb = arr.filter(item => item.includes('R'))
+// console.log(hasRs, hasRb)
